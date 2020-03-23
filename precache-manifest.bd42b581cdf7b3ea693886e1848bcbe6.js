d7e8e09fc4a29f5811fd36864e7779842b6a4e18
Leonardo Acevedo <leo.acevedo@gmail.com>
2020-03-23 01:50:42 -0300

    Updates

---
+++ precache-manifest.bd42b581cdf7b3ea693886e1848bcbe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b467379a3f3f8802fcf27540679141",
+    "url": "/react-flashcards/index.html"
+  },
+  {
+    "revision": "07bbe6153cf14b250ea1",
+    "url": "/react-flashcards/static/css/main.0b2b6ba6.chunk.css"
+  },
+  {
+    "revision": "73bffd21758d0dbad075",
+    "url": "/react-flashcards/static/js/2.ca3187d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flashcards/static/js/2.ca3187d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bbe6153cf14b250ea1",
+    "url": "/react-flashcards/static/js/main.6412d942.chunk.js"
+  },
+  {
+    "revision": "f570f69aff8669e57a8d",
+    "url": "/react-flashcards/static/js/runtime-main.250e328b.js"
+  }
+]);
