23e39c827742ad3bb15596924ee9b5826d1f064f
Leonardo Acevedo <leo.acevedo@gmail.com>
2020-03-31 01:28:54 -0300

    Reduce scroll sensivity

--- src/components/pager/index.js
+++ src/components/pager/index.js
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types'
 import './styles.css'
 
 class Pager extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.onWheel = this.onWheel.bind(this);
+      this.wheelEvtCount = 0
+    }
     reference(id) {
       return ref => { this[id] = ref }
     }
@@ -11,10 +17,18 @@ class Pager extends React.Component {
       this.scroll.scrollLeft = 0
     }
 
+    componentDidMount() {
+      this.scroll.addEventListener('wheel', this.onWheel)
+    }
+
     render() {
       return (
         <div>
-          <div className="pager" ref={this.reference('scroll')}>
+          <div 
+            className="pager" 
+            ref={this.reference('scroll')}
+          
+        >
             { 
               this.props.data.map(
                 elem => this.props.createView(
@@ -26,6 +40,13 @@ class Pager extends React.Component {
           </div>
         </div>
       );
+    }
+
+    onWheel(evt) {
+      this.wheelEvtCount = (this.wheelEvtCount + 1) % 15;
+      if (this.wheelEvtCount !== 0 || Math.abs(evt.deltaX) > 20) {
+        evt.preventDefault();
+      }      
     }
 }  
 
