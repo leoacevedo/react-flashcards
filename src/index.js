689ec07dd49386380af4a8a60605067a2f7deee7
Leonardo Acevedo <leo@leoacevedo.com>
2026-06-17 14:24:05 -0300

    Agregar deck para UNPaz

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App.js'
 
 const deckKeys = [
+  { key: '汉语研讨会', value: 'unpaz-yantaohui-1' },
   { key: 'HSK1', value: 'hsk1' },
-  { key: 'HSK2', value: 'hsk2' },
-  { key: 'HSK3', value: 'hsk3' },
-  { key: 'HSK4', value: 'hsk4' },
-  { key: 'HSK5', value: 'hsk5' },
-  { key: 'HSK6', value: 'hsk6' },
-  { key: 'Labo 1', value: 'labo1' },
-  { key: '幼儿中文1', value: 'kids' },
 ];
 
 ReactDOM.render(
